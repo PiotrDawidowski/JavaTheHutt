3320b139fa5e8f6f6e61f939c38e7082592be989
Piotr <pepedawidowski@gmail.com>
2025-08-05 16:02:40 +0200

    Java Tuto ep. 5

---
+++ src/JavaTutoClasses/Arithmetics_5.java
@@ -0,0 +1,23 @@
+package JavaTutoClasses;
+
+public class Arithmetics_5 {
+    public void Arithmetics() {
+        int x = 10;
+        int y = 2;
+        int z = 0;
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y; // better use doubles
+//        x %= y;
+
+        x++;
+        System.out.println(z);
+    }
+}
