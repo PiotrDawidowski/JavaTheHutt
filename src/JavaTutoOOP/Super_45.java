f701b438ca8dd17aa3a8ed26e4f51562870868e9
PiotrDawidowski <pepedawidowski@gmail.com>
2025-09-22 05:39:19 +0200

    Super

---
+++ src/JavaTutoOOP/Super_45.java
@@ -0,0 +1,47 @@
+package JavaTutoOOP;
+
+public class Super_45 {
+    public static class Person {
+        public String first;
+        public String last;
+
+        public Person() {
+        }
+
+        public Person(String first, String last) {
+            this.first = first;
+            this.last = last;
+        }
+
+        public void showName() {
+            System.out.println(first + " " + last);
+        }
+    }
+
+    // every field parent constructor requires, we need to pass it to it
+    public static class Student extends Person {
+        public double gpa;
+
+        public Student(String first, String last, double gpa) {
+            super(first, last);
+            this.gpa = gpa;
+        }
+
+        public void showGPA() {
+            System.out.println("GPA: " + this.gpa);
+        }
+    }
+
+    public static class Employee extends Person {
+        public int salary;
+        public Employee(String first, String last, int salary) {
+            super(first, last);
+            this.salary = salary;
+        }
+
+        public void showSalary() {
+            System.out.println("Salary: " + this.salary);
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -193,12 +193,22 @@ public class Main {
 //        Static_43.Friend.showFriends();
 
         // 44
-        Inheritance_44 inh44 = new Inheritance_44();
-        Inheritance_44.Dog dog = new Inheritance_44.Dog();
-        Inheritance_44.Cat cat = new Inheritance_44.Cat();
-        Inheritance_44.Plant plant = new Inheritance_44.Plant();
+//        Inheritance_44 inh44 = new Inheritance_44();
+//        Inheritance_44.Dog dog = new Inheritance_44.Dog();
+//        Inheritance_44.Cat cat = new Inheritance_44.Cat();
+//        Inheritance_44.Plant plant = new Inheritance_44.Plant();
+//
+//        plant.photosynthesize();
 
-        plant.photosynthesize();
+        // 45
+        Super_45 sup45 = new Super_45();
+        Super_45.Person person1 = new Super_45.Person("Tom", "Hanks");
+        person1.showName();
+        Super_45.Student student1 = new Super_45.Student("Shinji", "Hirako", 3.7);
+        student1.showName(); student1.showGPA();
+        Super_45.Employee employee1 = new Super_45.Employee("Marcin", "Kowalski", 3899);
+        employee1.showName(); employee1.showSalary();
+
     }
 }
 
