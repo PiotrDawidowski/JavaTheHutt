6c3aa58dd1fd951be914349d0992461cad301378
Piotr Dawidowski <pepedawidowski@gmail.com>
2025-10-21 03:26:38 +0200

    Composition

--- src/JavaTutoOOP/Composition_54/Car_54.java
+++ src/JavaTutoOOP/Composition_54/Car_54.java
@@ -1,4 +1,13 @@
 package JavaTutoOOP.Composition_54;
 
 public class Car_54 {
+    public String model;
+    public int year;
+    public Engine_54 engineType;
+
+    public Car_54(String model, int year, String engineType) {
+        this.model = model;
+        this.year = year;
+        this.engineType = new Engine_54(engineType);
+    }
 }
--- src/JavaTutoOOP/Composition_54/Engine_54.java
+++ src/JavaTutoOOP/Composition_54/Engine_54.java
@@ -1,4 +1,13 @@
 package JavaTutoOOP.Composition_54;
 
 public class Engine_54 {
+    public String type;
+
+    Engine_54(String type) {
+        this.type = type;
+    }
+
+    public void start() {
+        System.out.println("You start the " + this.type);
+    }
 }
